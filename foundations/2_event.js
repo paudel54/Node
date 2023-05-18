2b8f534599e142cafdc6faf87f1973946e319d99
paudel54 <2sansrit@gmail.com>
2023-05-18 17:28:50 +0545

    Worked on with node event emmiter

--- foundations/2_event.js
+++ foundations/2_event.js
@@ -3,14 +3,22 @@ const celebrity = new EventEmitter();
 
 //Suscribe to Celebrity for Observer 1
 //first argument followed up with callBack fn
-celebrity.on('race win', () => {
-    console.log('Congratulations You are best!!!');
+celebrity.on('race ', (result) => {
+    if (result === 'win') {
+        console.log('Congratulations You are best!!!');
+    }
 });
 
 //Suscribe to celebrity for Observer 2
-celebrity.on('race lost', () => {
-    console.log('I will work harder!!');
+celebrity.on('race', (result) => {
+    if (result === 'lost') {
+        console.log('I will work harder!!');
+    }
 });
 
-celebrity.emit('race win');
-celebrity.emit('race lost');
+process.on('exit', (code) => {
+    console.log('Process exit event with code:', code);
+})
+
+celebrity.emit('race', 'win');
+celebrity.emit('race', 'lost');
