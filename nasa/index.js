adf69789e3bbee9c48b51bd3c30da64a77c148f0
paudel54 <2sansrit@gmail.com>
2023-05-23 05:16:40 +0545

    Creating a readable JSON Object file for CSV with Node .pipe() method

--- nasa/index.js
+++ nasa/index.js
@@ -1,1 +1,27 @@
-const parse = require('csv-parse');
+const { parse } = require('csv-parse');
+const fs = require('fs');
+
+const results = [];
+
+// data, error, end are events of createReadStream: see Docs
+fs.createReadStream('kepler_data.csv')
+    // use of pipe to connect : pipe fn connect readeable stream src to writable stream destination parse()
+    // kepler_data is src and parse() is destination for pipe
+    .pipe(parse(
+        {
+            comment: '#',
+            columns: true,
+        }
+    ))
+    .on('data', (data) => {
+        results.push(data);
+    })
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on('end', (data) => {
+        console.log(results);
+        console.log('done');
+    });
+
+// Parse
