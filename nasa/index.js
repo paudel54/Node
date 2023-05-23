96fa0036430f6adfcf4c8df8d47ac4bd940d2529
paudel54 <2sansrit@gmail.com>
2023-05-23 05:51:22 +0545

    Worked with CSV file implemented read stream to find habitable exo planets

--- nasa/index.js
+++ nasa/index.js
@@ -3,6 +3,16 @@ const fs = require('fs');
 
 const results = [];
 
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.16
+
+}
+
+// insolation flux amount of light to planet
+// planatery radius is also limit to habitable palent
+
 // data, error, end are events of createReadStream: see Docs
 fs.createReadStream('kepler_data.csv')
     // use of pipe to connect : pipe fn connect readeable stream src to writable stream destination parse()
@@ -14,14 +24,20 @@ fs.createReadStream('kepler_data.csv')
         }
     ))
     .on('data', (data) => {
-        results.push(data);
+        if (isHabitablePlanet(data)) {
+            results.push(data);
+        }
+
     })
     .on('error', (err) => {
         console.log(err);
     })
-    .on('end', (data) => {
-        console.log(results);
-        console.log('done');
-    });
 
-// Parse
+    // Parse
+
+    .on('end', () => {
+        console.log(results.map((planet) => {
+            return planet['kepler_name'];
+        }));
+        console.log(`${results.length} habitable planets found!`);
+    });
