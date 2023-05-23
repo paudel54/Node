8c4939c581155fc4c00ed6dff4f3bbe1e87fcc29
paudel54 <2sansrit@gmail.com>
2023-05-23 08:05:59 +0545

    Worked on with HTTP API and Routings

--- http-server/index.js
+++ http-server/index.js
@@ -3,23 +3,54 @@ const PORT = 3000;
 // callback takes request listener
 // request handles header and data, true for response
 // req is readable stream can read incoming data, res is writeable. 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'application/json'
-    })
-    res.end(
-        JSON.stringify(
-            {
-                id: 5,
-                name: 'Balen',
-                description: 'Hello form the web development Agency, You are hired for our Job'
-            }
-        )
-    )
-    // passing data to browser with res.end()
+const server = http.createServer(); //127.0.0.1 => localhost
 
-});
+const friends = [
+    {
+        id: 0,
+        name: 'Nikola Tesla',
+    },
+    {
+        id: 1,
+        name: 'Sir Isaac Newton',
+    },
+    {
+        id: 2,
+        name: 'Albert Einstein',
+    }
+];
+
+server.on('request', (req, res) => {
+    if (req.url === '/friends') {
+        // res.writeHead(200, {
+        //     'Content-Type': 'application/json',
+        // });
+        // equivalent code
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(
+            {
+                id: 1,
+                name: 'Newton'
+            }
+        ))
+    } else if (req.url === '/message') {
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li>Hello Isaac!</li>');
+        res.write('<li>What are your thoughts on astronomy?</li>');
+        res.write('</ul>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
+})
 
 server.listen(PORT, () => {
-    console.log(`listening on port ${PORT}....`)
+    console.log(`Listening on port ${PORT}...`);
 }); //127.0.0.1 => localhost
