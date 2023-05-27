eace4ad2deefd9f90ad7c561cfca4b6dc2674218
paudel54 <2sansrit@gmail.com>
2023-05-27 11:11:20 +0545

    Worked with template engine to pass dynamic value to staic page with hbs

--- 8_express/server.js
+++ 8_express/server.js
@@ -1,6 +1,10 @@
 // Establishing Server with Express::=>  
 const express = require('express');
+const path = require('path');
 const app = express();
+// added tepmlating engines handlebar. 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 const PORT = 3000;
 // const friendsController = require('./controllers/friends.controller');
 // const messagesController = require('./controllers/messages.controller');
@@ -23,13 +27,22 @@ app.use((req, res, next) => {
 // express.json() is a middleware if no element it puts it as empty obj
 // serving website with node. 
 // possible url would be locahost:3000/site/index.html
-app.use('/site', express.static('public'));
+// always onto static file path routes. 
+// express.static is used to serve the static files. 
+app.use('/site', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 // working with express router. Organized onto folder. 
 // const friendsRouter = express.Router();
 
 //server doesn't understands json so needed to be parsed. 
+app.get('/', (req, res) => {
+    // returning template with dynamic html handlers. 
+    res.render('index', {
+        title: 'Personal Express Project.',
+        caption: 'Let\'s Get this done!'
+    })
+})
 app.use('/friends', friendsRouter);
 app.use('/messages', messagesRouter);
 // simplifying routes path with express router. 
@@ -37,7 +50,7 @@ app.use('/messages', messagesRouter);
 // handling dynamic routing with colon :  expess provides a method
 // Everything after the colon will be extracted. as dynamic routing features. 
 // paramaterized routes: /friends/22   
-
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}...`);
-}); 
+});
+
